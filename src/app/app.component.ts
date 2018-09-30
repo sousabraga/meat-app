41f293113c18ee630cdbc8c3cf2ad84dace4b190
Matheus Braga <matheusssbraga@gmail.com>
2018-09-30 18:34:00 -0300

    Criando os componentes Header e Home

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,11 +6,8 @@ import {Component, OnInit} from "@angular/core"
 })
 export class AppComponent implements OnInit {
 
-  content = 'Welcome do Meat App!'
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
 }
