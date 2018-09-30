311b8669a45dd438bfc99f9be2851e0d0bf11bd1
Matheus Braga <matheusssbraga@gmail.com>
2018-09-30 19:04:54 -0300

    Criando o componente About e iniciando a configuração das rotas

---
+++ src/app/about/about.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'mt-about',
+  templateUrl: './about.component.html',
+})
+export class AboutComponent implements OnInit {
+
+  constructor() {}
+
+  ngOnInit() {}
+
+}
