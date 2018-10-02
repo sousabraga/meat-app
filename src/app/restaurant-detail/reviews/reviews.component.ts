0d7780ea2a65df665e96e55592e56cd707545b9c
Matheus Braga <matheusssbraga@gmail.com>
2018-10-02 16:51:33 -0300

    Implementando sub-navegação na página de detalhe de restaurante para navegar entre menu e avaliações

---
+++ src/app/restaurant-detail/reviews/reviews.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'mt-reviews',
+  templateUrl: './reviews.component.html'
+})
+export class ReviewsComponent implements OnInit {
+
+  constructor() {}
+
+  ngOnInit() {}
+
+}
