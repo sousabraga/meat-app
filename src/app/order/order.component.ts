881a28ff1f5bfd65c29f79bb40b79fefbdd40bb0
Matheus Braga <matheusssbraga@gmail.com>
2018-10-09 17:30:57 -0300

    Aplicando validação para verificar se os emails são exatamente iguais

--- src/app/order/order.component.ts
+++ src/app/order/order.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators, AbstractControl } from '@angular/forms';
 import { Router } from '@angular/router';
 
 import { RadioOption } from '../shared/radio/radio-option.model';
@@ -42,7 +42,22 @@ export class OrderComponent implements OnInit {
       number: this.formBuilder.control("", [Validators.required, Validators.pattern(this.numberPattern)]),
       optionalAddress: this.formBuilder.control(""),
       paymentOption: this.formBuilder.control("", [Validators.required])
-    });
+    }, {validator: OrderComponent.equalsTo});
+  }
+
+  static equalsTo(group: AbstractControl): {[key: string]: boolean} {
+    const email = group.get("email");
+    const emailConfirmation = group.get("emailConfirmation");
+
+    if (!email || !emailConfirmation) {
+      return undefined;
+    }
+
+    if (email.value !== emailConfirmation.value) {
+      return {emailsNotMatch: true};
+    }
+
+    return undefined;
   }
 
   cartItems(): CartItem[] {
